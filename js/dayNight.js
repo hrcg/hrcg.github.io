b62d0dd8e2eb9fadb12052bdffc984a5e6124655
hrcg <perrandor@gmail.com>
2021-09-23 21:13:22 +0200

    made dark mode the dafault mode

--- js/dayNight.js
+++ js/dayNight.js
@@ -3,15 +3,15 @@ const wrapper = document.querySelector(".dayNight");
 const toggle = wrapper.querySelector("input");
 
 if (localStorage.getItem("mode") == null) {
-  localStorage.setItem("mode", "light-mode");
+  localStorage.setItem("mode", "dark-mode");
 }
 
 body.classList.value = localStorage.getItem("mode");
-toggle.checked = localStorage.getItem("mode") === "light-mode" ? true : false;
+toggle.checked = localStorage.getItem("mode") === "dark-mode" ? true : false;
 wrapper.classList.remove("hidden");
 
 toggle.addEventListener("change", () => {
-  const mode = toggle.checked ? "light-mode" : "dark-mode";
+  const mode = toggle.checked ? "dark-mode" : "light-mode";
   body.classList.value = mode;
   localStorage.setItem("mode", mode);
 });
