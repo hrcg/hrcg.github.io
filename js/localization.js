09c6e45bdcd55efebb3536de11a0c3254ab900eb
hrcg <perrandor@gmail.com>
2021-09-24 19:14:37 +0200

    added files for future localization

---
+++ js/localization.js
@@ -0,0 +1,33 @@
+/*let english = document.getElementById("english");
+let albanian = document.getElementById("albanian");
+let hello = document.getElementById("hello");
+let jrDev = document.getElementById("jrdev");
+let hire = document.getElementById("hire");
+
+english.onclick = () => {
+  setLanguage("english");
+  localStorage.setItem("Lang", "english");
+};
+
+albanian.onclick = () => {
+  setLanguage("albanian");
+  localStorage.setItem("Lang", "albanian");
+};
+
+onload = () => {
+  setLanguage(localStorage.getItem("Lang"));
+};
+function setLanguage(getLanguage) {
+  if (getLanguage === "english") {
+    hello.innerHTML = "👋 Hi, I'm Erald Kuci.";
+    jrDev.innerHTML =
+      "I'm a junior Web Developer based in Albania who likes to code and solve problems.";
+    hire.innerHTML = "I am also available for hire!";
+  } else if (getLanguage === "albanian") {
+    hello.innerHTML = "👋 Ç'kemi, unë jam Erald Kuci.";
+    jrDev.innerHTML =
+      "Jam një junior Web Developer i bazuar në Shqipëri, dhe më pëlqen programimi dhe zgjidhja e problemeve.";
+    hire.innerHTML = "Gjithashtu jam i disponueshëm për punë!";
+  }
+}
+*/
