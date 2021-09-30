d1b2f367196191585711036177d12c8917fb1b6e
hrcg <perrandor@gmail.com>
2021-09-30 03:27:12 +0200

    update

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,18 @@
-function menuToggle() {
-  var nav = document.getElementById("nav");
-  var toggle = document.getElementById("toggle");
-  nav.classList.toggle("active");
-  toggle.classList.toggle("inactive");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
